6741fe344abffcaa9deb38715adc0b5af34f5866
Francielly <139650531+Zebraf1sh@users.noreply.github.com>
2023-12-21 14:17:48 -0300

    Add files via upload

---
+++ Desafio_de_Projeto_1.rb
@@ -0,0 +1,7 @@
+print "Digite o seu nome "
+nome = gets.chomp
+print = "Digite o seu sobrenome "
+sobrenome = gets.chomp
+print = "Digite a sua idade "
+idade = gets.chomp
+puts "Olá #{nome} #{sobrenome}! Seja bem vindo! Sua idade é #{idade}."
